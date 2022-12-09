b31b2eb10110fd9e1851832ad96aa314bd2af7dd
HeeYeonKim98 <heeeee0818@gmail.com>
2022-12-09 14:49:53 +0900

    [feat] setting config

---
+++ src/constants/db.constant.ts
@@ -0,0 +1,4 @@
+export default {
+  USERS_REPOSITORY: 'Users',
+  POSTS_REPOSITORY: 'Posts',
+};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,6 @@
+/** comm */
 export { default as Configuration } from './configuration';
 
+/** constant */
+export { default as DB } from './db.constant';
 export { default as Swagger } from './swagger.constant';
